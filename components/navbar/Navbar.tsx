c6ab945400567d6a824206ca740f907ae661806e
Martin-Marelius <johnsen.martin1@googlemail.com>
2022-02-05 18:34:10 +0100

    added navbar css

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 
 const Navbar = () => {
     return (
-        <div className="flex-row gap-x-8 justify-items-end ">
-            <div>Portfolio</div>
-            <div>About</div>
-            <div>Contact</div>
+        <div className='flex flex-row relative place-content-end gap-5 mr-20'>
+            <span className="flex gap-10 bg-gradient-to-l from-orange-400 to-rose-400 rounded-full p-1 px-5 mt-5">
+                <p className='font-bold font-mono px-4 text-lg text-white'>Portfolio</p>
+                <p className='font-bold font-mono px-4 text-lg text-white'>About</p>
+                <p className='font-bold font-mono px-4 text-lg text-white'>Contact</p>
+            </span>
         </div>
     );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import { Footer, Header, Navbar, Display } from '../components';
 
 const IndexPage = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <Footer />
+  <div className='relative bg-gradient-to-br from-rose-400 to-orange-300'>
+
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+
     <Display />
+    <Footer />
   </div>
 
 )
 
 export default IndexPage
+
